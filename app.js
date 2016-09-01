77b378875a12f4b25ba0ad56a541bcd103aaae17
quantumarchive <chrbruner9@gmail.com>
2016-08-31 17:02:28 -0700

    store information to localStorage so refresh doesn't reset data collected

--- app.js
+++ app.js
@@ -4,16 +4,31 @@ var imageDirStr = [ 'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'break
                     'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg',
                     'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png',
                     'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
+
+//indexArray will hold numbers from 0 to N-1 where N is the size of the imageDirStr Array
+//it will hold the indexes related to a given image that corresponds to objects with that image
 var indexArray = [];
+
+//will hold just the name of the image after split is run on the imageDirStr
 var imageNameArray = [];
+
+//will hold the indexes pertaining to given 3 images to be rendered to the page
 var indexObjectCreate = [];
+
+//will hold all instantiated objects after iterating over imageDirStr
 var storeObjectArray = [];
+
+//will store all chartObjects that are initiated
 var chartObjectArray = [];
+
+//will hold all corresponding statistics relavent to the page and the index of each array corresponds to the corresponding image/object in imageDirStr or storeObjectArray
 var statDictionary = {
   clickStats : Array(imageDirStr.length).fill(0),
   viewStats : Array(imageDirStr.length).fill(0),
   clicksPerViewStatsArray : Array(imageDirStr.length).fill(0),
 };
+
+//keeps track of total clicks on the image
 var totalClicks = 0;
 
 function ImageObject(name,path) {
@@ -22,14 +37,6 @@ function ImageObject(name,path) {
   this.totalClicks = 0;
   this.totalViews = 0;
   storeObjectArray.push(this);
-};
-
-ImageObject.prototype.incrementClicks = function() {
-  this.totalClicks += 1;
-};
-
-ImageObject.prototype.incrementViews = function() {
-  this.totalViews += 1;
 };
 
 function initializeObjectsIndex() {
@@ -71,14 +78,18 @@ function calculateViews(objectIndexedArrayValues) {
   //pass array with index numbers for objects that were selected for viewing on page
   //and increment their views and calculate the clicks/view of a given object
   for (var i = 0; i < objectIndexedArrayValues.length; i++) {
-    storeObjectArray[objectIndexedArrayValues[i]].incrementViews();
+    storeObjectArray[objectIndexedArrayValues[i]].totalViews += 1;
     statDictionary.viewStats[objectIndexedArrayValues[i]] += 1;
-    statDictionary.clicksPerViewStatsArray[objectIndexedArrayValues[i]] = statDictionary.clickStats[objectIndexedArrayValues[i]] / statDictionary.viewStats[objectIndexedArrayValues[i]];
+    if (statDictionary.viewStats[objectIndexedArrayValues[i]] === 0) {
+      statDictionary.clicksPerViewStatsArray[objectIndexedArrayValues[i]] = 0;
+    } else {
+      statDictionary.clicksPerViewStatsArray[objectIndexedArrayValues[i]] = statDictionary.clickStats[objectIndexedArrayValues[i]] / statDictionary.viewStats[objectIndexedArrayValues[i]];
+    }
   };
 };
 
 function calculateClicks(index) {
-  storeObjectArray[index].incrementClicks();
+  storeObjectArray[index].totalClicks += 1;
   statDictionary.clickStats[index] += 1;
   statDictionary.clicksPerViewStatsArray[index] = statDictionary.clickStats[index] / statDictionary.viewStats[index];
   totalClicks += 1;
@@ -124,21 +135,35 @@ function barChartDraw(domElement, array, name) {
   //create chart element and push it to chartObjectArray so the objects can be updated
   //later if we want to update live
   var newLabel = '# of ' + name;
+  var canvasContext = domElement.getContext('2d');
+  var gradient = canvasContext.createLinearGradient(0,0,0,460);
+  gradient.addColorStop(0, 'rgba(60, 179, 113, 0.8)');
+  gradient.addColorStop(1, 'rgba(25, 25, 112, 0.8)');
+  var dataSetsDict = {
+    label: newLabel,
+    data: array,
+    backgroundColor: gradient,
+    boderWidth: 1,
+  };
   var chartElement = new Chart(domElement, {
     type: 'bar',
     data: {
       labels: imageNameArray,
-      datasets: [{
-        label: newLabel,
-        data: array,
-        borderWidth: 1,
-      }],
+      datasets: [dataSetsDict],
     },
     options: {
       scales: {
         yAxes: [{
           ticks: {
             beginAtZero: true,
+            fontSize: 20,
+            fontFamily: 'sans-serif',
+          }
+        }],
+        xAxes: [{
+          ticks: {
+            fontSize: 20,
+            fontFamily: 'sans-serif',
           }
         }]
       }
@@ -149,18 +174,23 @@ function barChartDraw(domElement, array, name) {
 
 function drawCharts() {
   //draws charts onto html page
-  var clickElement = document.getElementById('click_stats');
-  var viewElement = document.getElementById('view_stats');
-  var clickViewRatio = document.getElementById('click_view_stats');
-  var domElArray = [clickElement, viewElement, clickViewRatio];
-  var domElName = ['Clicks', 'Views', 'Clicks/Views'];
   var loopCounter = 0;
+  //calculateClicksPerViews();
+
   if (chartObjectArray.length === 0) {
+    var clickElement = document.getElementById('click_stats');
+    var viewElement = document.getElementById('view_stats');
+    var clickViewRatio = document.getElementById('click_view_stats');
+    var domElArray = [clickElement, viewElement, clickViewRatio];
+    var domElName = ['Clicks', 'Views', 'Clicks/Views'];
+
     for (var key in statDictionary) {
       barChartDraw(domElArray[loopCounter], statDictionary[key], domElName[loopCounter]);
       loopCounter += 1;
     };
+
   } else {
+
     for (var rekey in statDictionary) {
       chartObjectArray[loopCounter].data.datasets[0].data = statDictionary[rekey];
       chartObjectArray[loopCounter].update();
@@ -201,26 +231,64 @@ function renderButton() {
 function clickHandler(event) {
   //get index value from image that was clicked and pass to calculateClicks
   //if total clicks reaches 25, stop the script
-  var index = Number(event.target.getAttribute('index'));
-  var newIndexObjectCreate;
-  calculateClicks(index);
-  //console.log(totalClicks);
-  if (checkClicksTotal(totalClicks)) {
-    drawCharts();
-    renderButton();
-  } else {
-    imageList.textContent = '';
-    newIndexObjectCreate = randomNumbArray((imageDirStr.length - 1), 0, 3);
-    calculateViews(newIndexObjectCreate);
-    drawImage(newIndexObjectCreate);
-    restoreIndexArray(indexObjectCreate);
-    indexObjectCreate = newIndexObjectCreate;
+  var eventCheck = event.target.getAttribute('src');
+  if (eventCheck !== null) {
+    var index = Number(event.target.getAttribute('index'));
+    var newIndexObjectCreate;
+    calculateClicks(index);
+    //console.log(totalClicks);
+    if (checkClicksTotal(totalClicks)) {
+      drawCharts();
+      renderButton();
+      //when you refresh your browser will store an array of indexes so you don't see the same images
+      newIndexObjectCreate = randomNumbArray((imageDirStr.length - 1), 0, 3);
+      restoreIndexArray(indexObjectCreate);
+      indexObjectCreate = newIndexObjectCreate;
+      setSavedData();
+    } else {
+      imageList.textContent = '';
+      newIndexObjectCreate = randomNumbArray((imageDirStr.length - 1), 0, 3);
+      calculateViews(newIndexObjectCreate);
+      drawImage(newIndexObjectCreate);
+      restoreIndexArray(indexObjectCreate);
+      indexObjectCreate = newIndexObjectCreate;
+    }
   }
 };
 
+function checkSavedData() {
+  //check if data is saved to localStorage and if it isn't initialize all arrays needed to begin storing
+  var dictStoredWebDataObjects = localStorage.getItem('webPageData');
+  var savedData;
+  if (dictStoredWebDataObjects === null) {
+    initializeObjectsIndex();
+    indexObjectCreate = randomNumbArray((imageDirStr.length - 1), 0, 3);
+  } else {
+    savedData = JSON.parse(dictStoredWebDataObjects);
+    indexArray = savedData['indexArrayKey'];
+    imageNameArray = savedData['imageNameArrayKey'];
+    storeObjectArray = savedData['storeObjectArrayKey'];
+    statDictionary = savedData['statDictionaryKey'];
+    indexObjectCreate = savedData['indexObjectCreateKey'];
+  };
+};
+
+function setSavedData() {
+  //save data to localStorage
+  var savedDataDictionary = {
+    indexArrayKey: indexArray,
+    imageNameArrayKey: imageNameArray,
+    storeObjectArrayKey: storeObjectArray,
+    statDictionaryKey: statDictionary,
+    indexObjectCreateKey: indexObjectCreate,
+  };
+  var savedDataDictionaryString = JSON.stringify(savedDataDictionary);
+  localStorage.setItem('webPageData', savedDataDictionaryString);
+};
+
 //main
-initializeObjectsIndex();
-indexObjectCreate = randomNumbArray((imageDirStr.length - 1), 0, 3);
+
+checkSavedData();
 calculateViews(indexObjectCreate);
 drawImage(indexObjectCreate);
 
